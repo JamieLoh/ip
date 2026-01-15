7724b278e66b7d86cbd052385833a4bf7e778bcf
JamieLoh <jamie.loh.my@hotmail.com>
2026-01-15 15:13:36 +0800

    Rename main file and add greeting

---
+++ src/main/java/Sophon.java
@@ -0,0 +1,16 @@
+public class Sophon {
+    public static void main(String[] args) {
+        String logo =
+                "  ____              _                 \n" +
+                        " / ___|  ___  _ __ | |__   ___  _ __  \n" +
+                        " \\___ \\ / _ \\| '_ \\| '_ \\ / _ \\| '_ \\ \n" +
+                        "  ___) | (_) | |_) | | | | (_) | | | |\n" +
+                        " |____/ \\___/| .__/|_| |_|\\___/|_| |_|\n" +
+                        "             |_|                      \n";
+
+        System.out.println("Hello, Here is Sophon!");
+        System.out.println(logo + "\n");
+        System.out.println("What can I do for you? \n");
+        System.out.println("Bye. Hope to see you again soon! \n");
+    }
+}
