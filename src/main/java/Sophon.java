3523f406af887b6d9fad6430ce102806359a73cd
JamieLoh <jamie.loh.my@hotmail.com>
2026-01-19 16:28:49 +0800

    Complete level-2: Add features of add and list

--- src/main/java/Sophon.java
+++ src/main/java/Sophon.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Sophon {
@@ -11,6 +13,22 @@ public class Sophon {
 
     private final String GREETING_MESSAGE = "Hello, here is Sophon! How can I help you? \n";
     private final String EXIT_MESSAGE = "Bye bye! Sophon hopes to see you again soon! :) \n";
+    private List<String> tasksList = new ArrayList<String>();
+
+    public void addTask(String task){
+        tasksList.add(task);
+        System.out.println("Added task: " + task + "\n");
+    }
+
+    public void listTasks(){
+        Integer counter = 1;
+        for (String task : tasksList) {
+            System.out.println(counter + ". " + task);
+            counter++;
+        }
+        System.out.println("\n");
+    }
+
 
     public void run(){
         // greeting message
@@ -21,7 +39,12 @@ public class Sophon {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
         while(!userInput.equals("bye")){
-            System.out.println(userInput + "\n");
+            // check whether to add or list task
+            if (userInput.equals("list")){
+                listTasks();
+            } else {
+                addTask(userInput);
+            }
             userInput = sc.nextLine();
         }
         sc.close();
