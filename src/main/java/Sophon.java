4773732460d8f8ac2f15ce4102a1810a45a1a9d1
JamieLoh <jamie.loh.my@hotmail.com>
2026-01-19 15:59:32 +0800

    Add echo feature and modified based on level 0 to make it more OOP

--- src/main/java/Sophon.java
+++ src/main/java/Sophon.java
@@ -1,16 +1,39 @@
-public class Sophon {
-    public static void main(String[] args) {
-        String logo =
-                "  ____              _                 \n" +
-                        " / ___|  ___  _ __ | |__   ___  _ __  \n" +
-                        " \\___ \\ / _ \\| '_ \\| '_ \\ / _ \\| '_ \\ \n" +
-                        "  ___) | (_) | |_) | | | | (_) | | | |\n" +
-                        " |____/ \\___/| .__/|_| |_|\\___/|_| |_|\n" +
-                        "             |_|                      \n";
+import java.util.Scanner;
 
-        System.out.println("Hello, Here is Sophon!");
-        System.out.println(logo + "\n");
-        System.out.println("What can I do for you? \n");
-        System.out.println("Bye. Hope to see you again soon! \n");
+public class Sophon {
+    private String logo =
+            "  ____              _                 \n" +
+                    " / ___|  ___  _ __ | |__   ___  _ __  \n" +
+                    " \\___ \\ / _ \\| '_ \\| '_ \\ / _ \\| '_ \\ \n" +
+                    "  ___) | (_) | |_) | | | | (_) | | | |\n" +
+                    " |____/ \\___/| .__/|_| |_|\\___/|_| |_|\n" +
+                    "             |_|                      \n";
+
+    private final String GREETING_MESSAGE = "Hello, here is Sophon! How can I help you? \n";
+    private final String EXIT_MESSAGE = "Bye bye! Sophon hopes to see you again soon! :) \n";
+
+    public void run(){
+        // greeting message
+        System.out.println(logo);
+        System.out.println(GREETING_MESSAGE);
+
+        // interact with user
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        while(!userInput.equals("bye")){
+            System.out.println(userInput + "\n");
+            userInput = sc.nextLine();
+        }
+        sc.close();
+
+        // exit message
+        System.out.println(EXIT_MESSAGE);
+    }
+
+
+
+    public static void main(String[] args) {
+        // run the chatbot Sophon
+        new Sophon().run();
     }
 }
