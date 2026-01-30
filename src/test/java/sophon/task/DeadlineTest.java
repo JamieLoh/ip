527a0ae4bb25cc73ebb4eca79914a2aeaf32889c
JamieLoh <jamie.loh.my@hotmail.com>
2026-01-30 15:01:20 +0800

    Complete A-JUnit in minimum level

--- src/test/java/sophon/task/DeadlineTest.java
+++ src/test/java/sophon/task/DeadlineTest.java
@@ -1,8 +1,32 @@
 package sophon.task;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+
+
+
+
+
+
 public class DeadlineTest {
+    @Test
+    public void getDeadline_validDeadline_returnsCorrectDeadline() {
+        LocalDateTime deadline = LocalDateTime.of(2026, 2, 28, 12, 30, 15);
+        Deadlines task = new Deadlines("submit project", deadline);
+
+        assertEquals(deadline, task.getDeadline());
+    }
+
+    @Test
+    public void toString_validDeadlineTask_correctStringReturned() {
+        LocalDateTime deadline = LocalDateTime.of(2026, 2, 28, 12, 30, 15);
+        Deadlines task = new Deadlines("submit project", deadline);
+
+        String expected = "[D][ ] submit project (by Feb 28 2026, 12:30:15)";
+        assertEquals(expected, task.toString());
+    }
 }
--- src/test/java/sophon/task/EventTest.java
+++ src/test/java/sophon/task/EventTest.java
@@ -1,4 +1,38 @@
 package sophon.task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+
 public class EventTest {
+    @Test
+    public void getStartTime_validEvent_returnsCorrectStartTime() {
+        LocalDateTime startTime = LocalDateTime.of(2026, 2, 28, 12, 30, 15);
+        LocalDateTime endTime = LocalDateTime.of(2026, 2, 28, 14, 30, 15);
+        Event event = new Event("group meeting", startTime, endTime);
+
+        assertEquals(startTime, event.getStartTime());
+    }
+
+    @Test
+    public void getEndTime_validEvent_returnsCorrectEndTime() {
+        LocalDateTime startTime = LocalDateTime.of(2026, 2, 28, 12, 30, 15);
+        LocalDateTime endTime = LocalDateTime.of(2026, 2, 28, 14, 30, 15);
+        Event event = new Event("group meeting", startTime, endTime);
+
+        assertEquals(endTime, event.getEndTime());
+    }
+
+    @Test
+    public void toString_validEvent_correctStringReturned() {
+        LocalDateTime startTime = LocalDateTime.of(2026, 2, 28, 12, 30, 15);
+        LocalDateTime endTime = LocalDateTime.of(2026, 2, 28, 14, 30, 15);
+        Event event = new Event("group meeting", startTime, endTime);
+
+        String expected = "[E][ ] group meeting (from Feb 28 2026, 12:30:15 to Feb 28 2026, 14:30:15)";
+        assertEquals(expected, event.toString());
+    }
 }
